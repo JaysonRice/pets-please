cb75cee4f5c19ed329dc3c161d41de460fafef51
root <root@DESKTOP-2KLJLBF.localdomain>
2020-04-30 16:24:48 -0400

    Initial commit

---
+++ src/components/PetsPlease.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+
+export default () => {
+
+    return (
+        <div>
+            <img src="https://res.cloudinary.com/dawhgtkqk/image/upload/v1588278178/RedCropped_xxl9bv.png" alt="pets please logo"/>
+        </div>
+    )
+}
